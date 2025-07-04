8a3715299d39d5d96c2037103376335f0231f6e7
Josh Caponigro <joshcaponigro@protonmail.com>
2025-07-03 23:12:55 -0500

    refactor

--- util/template.py
+++ util/template.py
@@ -2,7 +2,7 @@ from models import Option
 
 def make(option: Option) -> str:
     # TODO: Implement blockDomainsByList and blockCommonTrackingPatternsByRegex
-    script = f'''\
+    return f'''\
 from mitmproxy import http
 from typing import Dict, List
 
@@ -10,12 +10,12 @@ ALLOWED_HEADERS: List[str] = {option.allowedHeaders}
 HEADER_OVERRIDES: Dict[str, str] = {option.headerOverrides}
 
 def request(flow: http.HTTPFlow) -> None:
-    allowed_headers: set[str] = set(ALLOWED_HEADERS)
-    available_headers: list[str] = list(flow.request.headers.keys()) # type: ignore
-    for header in available_headers:
-        if header not in allowed_headers:
+    header_allowlist: set[str] = set(ALLOWED_HEADERS)
+    # Remove not allowlisted headers
+    for header in flow.request.headers:
+        if header not in header_allowlist:
             del flow.request.headers[header]
+    # Set header overrides
     for header, value in HEADER_OVERRIDES.items():
         flow.request.headers[header] = value
 '''
-    return script
